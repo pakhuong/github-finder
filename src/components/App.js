48dfc798902f09ddc843df2d9a0698ac1c07478b
khuong.pham <khuongpham@bocasay.com>
2018-12-13 22:26:10 +0700

    Save changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Grid } from 'react-bootstrap';
+
+import Menu from './Menu/Menu';
+import Home from './Home/Home';
 
 class App extends Component {
   render() {
     return (
-      
+      <BrowserRouter>
+        <div>
+          <Menu />
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Menu = () => {
     return (
-
+        <Navbar inverse>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <Link to='/'>Github Finder</Link>
+                </Navbar.Brand>
+            </Navbar.Header>
+            <Nav pullRight>
+                <NavItem eventKey={1} href='https://github.com/pakhuong/github-finder'>
+                    <FontAwesomeIcon icon='github' />
+                </NavItem>
+            </Nav>
+        </Navbar>
     );
 };
+
+export default Menu;
